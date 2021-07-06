0fb038e8cf51d55a020336c1276f995e8789aa25
Melinda <melinda.sava.td@gmail.com>
2021-07-06 15:40:55 +0200

    ref vs reactive example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,34 @@
 <template>
   <div class="home">
-    <p ref="p" @click="logRef">
-      My name is {{ name }} and I am {{ age }} years old.
-    </p>
-    <button @click="handleClick">click me</button>
-    <input type="text" v-model="name">
-    <button @click="age++">+</button>
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ person.name }} ◈ {{ person.age }}</p>
+    <button @click="updatePerson">Be younger</button>
+    <h2>Reactive</h2>
+    <p>{{ personTwo.name }} ◈ {{ personTwo.age }} ◈ {{ nameTwo }}</p>
+    <button @click="updatePersonTwo">Make younger</button>
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
+import { reactive, ref } from "@vue/reactivity";
 
 export default {
   name: "Home",
   setup() {
-    const p = ref(null);
+    const person = ref({ name: "Mely", age: 32 });
+    const personTwo = reactive({ name: "John", age: 37 });
+    let nameTwo = reactive("Juxiela");
 
-    const name = ref("mario");
-    const age = ref(30);
-
-    const handleClick = () => {
-      name.value = "louigi";
-      age.value = 45;
+    const updatePerson = () => {
+      person.value.age = 28;
     };
 
-    return { name, age, handleClick, p };
+    const updatePersonTwo = () => {
+      personTwo.age = 30;
+    }
+
+    return { person, updatePerson, personTwo, updatePersonTwo, nameTwo };
   },
 };
 </script>
