c9f8365be9305150afbb69ed4265980587152c80
Melinda <melinda.sava.td@gmail.com>
2021-07-06 15:55:20 +0200

    search field with refs and computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,29 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ person.name }} ◈ {{ person.age }}</p>
-    <button @click="updatePerson">Be younger</button>
-    <h2>Reactive</h2>
-    <p>{{ personTwo.name }} ◈ {{ personTwo.age }} ◈ {{ nameTwo }}</p>
-    <button @click="updatePersonTwo">Make younger</button>
+    <input type="text" v-model="search" />
+    <p>search term {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      <h3>{{ name }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
+import { ref } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
 
 export default {
   name: "Home",
   setup() {
-    const person = ref({ name: "Mely", age: 32 });
-    const personTwo = reactive({ name: "John", age: 37 });
-    let nameTwo = reactive("Juxiela");
+    const search = ref("");
+    const names = ref(["Mely", "John", "Tudor", "Darius", "Octavian", "Matei"]);
 
-    const updatePerson = () => {
-      person.value.age = 28;
-    };
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
 
-    const updatePersonTwo = () => {
-      personTwo.age = 30;
-    }
-
-    return { person, updatePerson, personTwo, updatePersonTwo, nameTwo };
+    return { names, search, matchingNames };
   },
 };
 </script>
