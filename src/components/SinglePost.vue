a1e7557cc4d3c54e5d392e4413ccb44b6a52362e
Melinda <melinda.sava.td@gmail.com>
2021-07-07 11:48:53 +0200

    using props in the composition API plus CR prettier ignore rule defined in eslintrc.js

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post">
+    <h2>{{ post.title }}</h2>
+    <p>{{ snippet }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "@vue/runtime-core";
+
+export default {
+  props: ["post"],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.text.substring(0, 100) + "....";
+    });
+    return { snippet };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,42 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search" />
-    <p>search term {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      <h3>{{ name }}</h3>
-    </div>
-    <button @click="stopAllWatch">stop watch</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
-import { computed, watch, watchEffect } from "@vue/runtime-core";
+import PostList from "../components/PostList.vue";
 
 export default {
   name: "Home",
+  components: {
+    PostList,
+  },
   setup() {
-    const search = ref("");
-    const names = ref(["Mely", "John", "Tudor", "Darius", "Octavian", "Matei"]);
+    const posts = ref([
+      {
+        title: "the last vampire",
+        date: "long time ago",
+        text: "bla bla vampire bla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam molestie nibh sit amet elementum. Nunc magna lorem, eleifend non sem nec, pellentesque feugiat lectus. Mauris felis est, feugiat ac tempus non, scelerisque ut purus. Sed lobortis erat nec felis fringilla, in mollis quam sollicitudin. In quis ullamcorper velit. Nullam tempus euismod massa id scelerisque. Nulla in tempor leo. Duis vel urna sed metus suscipit ultrices vel vel magna. Aenean sodales vehicula eros nec viverra. Cras a tempor libero, nec consequat est. Nam a vulputate dolor. Vivamus sed nisi congue, vulputate sapien eu, consequat nunc. Aliquam in lectus nec diam porta fermentum quis id nisi. Nullam facilisis quis dolor vitae euismod. Praesent interdum porttitor aliquam. Fusce at euismod velit. Cras non hendrerit velit. Fusce nisi elit, varius quis nunc nec, porttitor bibendum nisl. Mauris eu consectetur mi. Nullam congue tortor vitae nisl scelerisque sodales. Phasellus eu molestie risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque commodo faucibus dapibus. Cras viverra quis augue nec tempor. Nam rutrum volutpat tortor, vitae ornare arcu tristique vitae. Aenean mauris ex, pretium nec metus non, aliquet ultricies nibh. Integer condimentum enim turpis, ac vestibulum dolor mollis at. Curabitur malesuada massa lectus, sed malesuada ante volutpat non.",
+        id: 1,
+      },
+      {
+        title: "ultimate shit",
+        date: "every couple'a days",
+        text: "shitty shit shit shoot. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam molestie nibh sit amet elementum. Nunc magna lorem, eleifend non sem nec, pellentesque feugiat lectus. Mauris felis est, feugiat ac tempus non, scelerisque ut purus. Sed lobortis erat nec felis fringilla, in mollis quam sollicitudin. In quis ullamcorper velit. Nullam tempus euismod massa id scelerisque. Nulla in tempor leo. Duis vel urna sed metus suscipit ultrices vel vel magna. Aenean sodales vehicula eros nec viverra. Cras a tempor libero, nec consequat est. Nam a vulputate dolor. Vivamus sed nisi congue, vulputate sapien eu, consequat nunc. Aliquam in lectus nec diam porta fermentum quis id nisi. Nullam facilisis quis dolor vitae euismod. Praesent interdum porttitor aliquam. Fusce at euismod velit. Cras non hendrerit velit. Fusce nisi elit, varius quis nunc nec, porttitor bibendum nisl. Mauris eu consectetur mi. Nullam congue tortor vitae nisl scelerisque sodales. Phasellus eu molestie risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque commodo faucibus dapibus. Cras viverra quis augue nec tempor. Nam rutrum volutpat tortor, vitae ornare arcu tristique vitae. Aenean mauris ex, pretium nec metus non, aliquet ultricies nibh. Integer condimentum enim turpis, ac vestibulum dolor mollis at. Curabitur malesuada massa lectus, sed malesuada ante volutpat non.",
+        id: 2,
+      },
+      {
+        title: "rainbows and perfume",
+        date: "every day",
+        text: "red orange yellow green blue indigo violet and a nice touch of lavander. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam molestie nibh sit amet elementum. Nunc magna lorem, eleifend non sem nec, pellentesque feugiat lectus. Mauris felis est, feugiat ac tempus non, scelerisque ut purus. Sed lobortis erat nec felis fringilla, in mollis quam sollicitudin. In quis ullamcorper velit. Nullam tempus euismod massa id scelerisque. Nulla in tempor leo. Duis vel urna sed metus suscipit ultrices vel vel magna. Aenean sodales vehicula eros nec viverra. Cras a tempor libero, nec consequat est. Nam a vulputate dolor. Vivamus sed nisi congue, vulputate sapien eu, consequat nunc. Aliquam in lectus nec diam porta fermentum quis id nisi. Nullam facilisis quis dolor vitae euismod. Praesent interdum porttitor aliquam. Fusce at euismod velit. Cras non hendrerit velit. Fusce nisi elit, varius quis nunc nec, porttitor bibendum nisl. Mauris eu consectetur mi. Nullam congue tortor vitae nisl scelerisque sodales. Phasellus eu molestie risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque commodo faucibus dapibus. Cras viverra quis augue nec tempor. Nam rutrum volutpat tortor, vitae ornare arcu tristique vitae. Aenean mauris ex, pretium nec metus non, aliquet ultricies nibh. Integer condimentum enim turpis, ac vestibulum dolor mollis at. Curabitur malesuada massa lectus, sed malesuada ante volutpat non.",
+        id: 3,
+      },
+    ]);
 
-    const stopWatch = watch(search, () => {
-      console.log("watch search ran, search is:", search.value);
-    });
-
-    const stopEffect = watchEffect(() => {
-      console.log('watch effect ran', search.value);
-    });
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) =>
-        name.toLowerCase().includes(search.value.trim().toLowerCase())
-      );
-    });
-
-    const stopAllWatch = () => {
-      stopWatch();
-      stopEffect();
-    };
-
-    return { names, search, matchingNames, stopAllWatch };
+    return { posts };
   },
 };
 </script>
